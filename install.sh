9221440662acc8e72c28460067af644a498c5110
Alfredo Beaumont <abeaumont@github.com>
2022-11-28 16:49:47 +0100

    Fix installation script.

--- install.sh
+++ install.sh
@@ -7,7 +7,13 @@ sudo apt install -y emacs-nox ripgrep fd-find
 emacs --batch --eval "(progn (package-initialize) (add-to-list 'package-archives '(\"melpa\" . \"https://melpa.org/packages/\")) (package-refresh-contents) (package-install 'use-package))"
 
 ## Move the existing dotfiles.
-for f in .gitconfig .emacs.d
+for f in .gitconfig
 do
     mv $f ~/
+done
+
+## use-package installation already created the directory, move individual files instead.
+for f in .emacs.d
+do
+    mv $f ~/.emacs.d/
 done
